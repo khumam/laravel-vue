0c1cf82448a64c6b74a8de3ab26a30aba67cda61
briyanpr <briyan.priyo@gmail.com>
2023-06-03 13:45:29 +0700

    DB Seed

---
+++ library/database/seeders/PublisherSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Publisher;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PublisherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for ($i=0; $i < 5; $i++){
+            $publisher = new Publisher;
+
+            $publisher->name = $faker->company;
+            $publisher->email = $faker->email;
+            $publisher->phone_number = '0821'.$faker->randomNumber(8);
+            $publisher->address = $faker->address;
+
+            $publisher->save();
+        }
+    }
+}
