3b08b3b03010deffb2835c3755a37923b707ca4c
briyanpr <briyan.priyo@gmail.com>
2023-06-08 21:14:47 +0700

    Eloquent

--- library/app/Http/Controllers/HomeController.php
+++ library/app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
+use App\Models\Book;
+use App\Models\Publisher;
+use App\Models\Author;
+use App\Models\Catalog;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,13 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //$members = Member::with('user')->get();
+        //$books = Book::with('publisher')->get();
+        //$publishers = Publisher::with('books')->get();
+        //$catalogs = Catalog::with('books')->get();
+        //$authors = Author::with('books')->get();
+
+        //return $authors;
         return view('home');
     }
 }
--- library/database/migrations/2010_06_02_134005_create_members_table.php
+++ library/database/migrations/2010_06_02_134005_create_members_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->id();
             $table->string('name', 64);
-            $table->char('gender', 1);
+            $table->char('gender',1);
             $table->char('phone_number', 14);
             $table->text('address');
             $table->string('email', 64);
