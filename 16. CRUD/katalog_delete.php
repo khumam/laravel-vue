fe04818a7a29906cb2f72be3a41e0491168d7336
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-06-16 14:39:56 +0700

    up: revert

--- 16. CRUD/katalog_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("connect.php");
- 
-$nama = $_GET['nama'];
- 
-$result = mysqli_query($mysqli, "DELETE FROM katalog WHERE nama='$nama'");
-
-header("Location:katalog.php");
-?>
--- 17. CRUD Bootstrap/pengarang_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("connect.php");
- 
-$nama_pengarang = $_GET['nama_pengarang'];
- 
-$result = mysqli_query($mysqli, "DELETE FROM pengarang WHERE nama_pengarang='$nama_pengarang'");
-
-header("Location:pengarang.php");
-?>
