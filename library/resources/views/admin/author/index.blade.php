51af3f5a669c7f97262dde36a7f7fd6f29385b52
briyanpr <briyan.priyo@gmail.com>
2023-06-21 16:39:04 +0700

    CRUD - Vue #2

--- library/resources/views/admin/author/index.blade.php
+++ library/resources/views/admin/author/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('css')
 
+<!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset ('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset ('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset ('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+
 @endsection
 
 @section('content')
@@ -13,7 +18,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Author</h1>
+            <h1>Publisher</h1>
           </div>
         </div>
       </div><!-- /.container-fluid -->
@@ -26,8 +31,8 @@
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-                <h5>Data Author</h5>
-                <a href="#" @click="addData()" class = "btn btn-sm btn-primary pull-right">Create New Author</a>
+                <h5>Data Publisher</h5>
+                <a href="#" @click="addData()" class = "btn btn-sm btn-primary pull-right">Create New Publisher</a>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -40,7 +45,7 @@
                     <th>Address</th>
                     <th>Phone Number</th>
                     <th>Total Books</th>
-                    <!--<th>Action</th>-->
+                    <th>Action</th>
                   </tr>
                   </thead>
                   <tbody>
@@ -110,6 +115,27 @@
 @endsection
 
 @section('js')
+
+<!-- DataTables  & Plugins -->
+<script src="{{ asset ('assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+
+<script type="text/javascript">
+ $(function () {
+    $("#example2").DataTable();
+  });
+</script>
+
 
   <script type="text/javascript">
     var controller = new Vue({
