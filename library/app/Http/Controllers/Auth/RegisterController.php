449474e7250fad6095e3fff3b917deb49e5021d6
fayzisme <fayzisme@gmail.com>
2023-08-17 22:07:10 +0700

    edit login dan fungsi registrasi

--- library/app/Http/Controllers/Auth/RegisterController.php
+++ library/app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Http\Request;
+
 class RegisterController extends Controller
 {
     /*
@@ -62,12 +64,37 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
-    protected function create(array $data)
+    // protected function create(array $data)
+    // {
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //     ]);
+    // }
+
+    public function register(Request $request)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+
+        // dd($request);
+       $validator = $this->validator($request->all());
+
+       if ($validator->fails()) {
+            // Jika validasi gagal, Anda dapat melakukan tindakan yang sesuai, seperti mengembalikan pesan error
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+       $data = $request->all();
+        //    dd($data);
+
+
+        User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+
+
+        return redirect("/login");
     }
 }
--- library/resources/views/auth/register.blade.php
+++ library/resources/views/auth/register.blade.php
@@ -12,46 +12,49 @@
   
         <form action="{{ route('register') }}" method="post">
           @csrf
-          
+
           <div class="input-group mb-3">
             <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Full Name">
-            @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+            
             <div class="input-group-append">
               <div class="input-group-text">
                 <label for="name" class="col-md-4 col-form-label text-md-end" style="display: none;"></label>
                 <span class="fas fa-user"></span>
               </div>
             </div>
-          </div>
-          <div class="input-group mb-3">
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
-            @error('email')
+            @error('name')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
+          </div>
+          <div class="input-group mb-3">
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+            
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
               </div>
             </div>
-          </div>
-          <div class="input-group mb-3">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
-            @error('password')
+            @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
+          </div>
+          <div class="input-group mb-3">
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
+            
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
               </div>
             </div>
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
           </div>
           <div class="input-group mb-3">
             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Retype password">
